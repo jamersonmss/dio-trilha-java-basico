8502f157686db54140e33fa5514022ac5dd0891a
jamersonmss <jamersonmssilva@gmail.com>
2023-01-27 12:18:48 -0300

    Atualização de números

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,7 +3,10 @@ public class Operadores {
      public static void main(String[] args) {
         int numero = 5;
 
-        System.out.println(-numero);
+        numero = -numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
 
         System.out.println(numero);
 
